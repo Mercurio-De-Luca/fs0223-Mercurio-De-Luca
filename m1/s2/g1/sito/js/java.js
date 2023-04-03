4158ba37096b5b99fb7286e123cd854708c25212
Mercurio-De-Luca <mirkodeluca413@gmail.com>
2023-04-03 16:43:10 +0200

    s2g1

---
+++ m1/s2/g1/sito/js/java.js
@@ -0,0 +1,28 @@
+function saluto(){
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world'); 
+}
+saluto()
+saluto()
+
+
+document.getElementById('testo').innerHTML='Ciao a tutti!!' 
+console.log(56+15)
+alert(56+15)
+var numero = 25
+var number = 50
+
+console.log(numero + number)
+
+var prezzo = 18 
+var quantità = 2
+console.log(prezzo * quantità) 
+
+var nome = 'Giovanni'
+var lavoro = 'Meccanico'
+var daQuantiAnni = 15
+ 
+document.write(nome + ' fa '+ ' il '+ lavoro +' da ' + ' 15 '+ ' anni ')
