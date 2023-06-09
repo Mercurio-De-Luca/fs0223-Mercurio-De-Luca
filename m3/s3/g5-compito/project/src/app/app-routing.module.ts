c4ba4ce83671084db8c621dd8043e603eff40d45
Mercurio De Luca <mirkodeluca413@gmail.com>
2023-06-09 16:59:47 +0200

    g5

---
+++ m3/s3/g5-compito/project/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { AuthGuard } from './pages/auth/auth.guard';
+
+const routes: Routes = [
+  { path:'', redirectTo: 'auth',pathMatch: 'full'},
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'post', loadChildren: () => import('./pages/post/post.module').then(m => m.PostModule),
+    canActivate: [AuthGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
